74078bc5d5c1e0382397679423ea720db0178494
shahriar021 <shahriarc19@gmail.com>
2025-06-23 20:20:27 +0600

    insertion

---
+++ sorting/insertion.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+void insertion(int* arr,int n){
+    int temp,min;
+    
+    for(int i=0;i<n-1;i++){
+        min =i;
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                min=j;
+            }
+            
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
+
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main() {
+    int n=5;
+    int arr[n]={5,9,8,7,1};
+    insertion(arr,n);
+    return 0;
+}
