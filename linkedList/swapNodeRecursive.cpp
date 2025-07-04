6ff4f0d9ef99cb5c423a163c22762a21e88b57e8
shahriar021 <shahriarc19@gmail.com>
2025-07-04 18:19:35 +0600

    swap nodes with recursive approach in linkedlis

---
+++ linkedList/swapNodeRecursive.cpp
@@ -0,0 +1,14 @@
+Listnode* swapPairs(ListNode* head){
+
+
+
+    if (!head || !head->next) return head;
+
+    ListNode* first = head;
+    ListNode* second = head->next;
+
+    first->next = swapPairs(second->next);
+    second->next = first;
+
+    return second;
+}
